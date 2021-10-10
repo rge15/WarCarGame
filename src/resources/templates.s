9b97a2acda7de3cbf45881992e2e3a591e2abec3
rge15 <78486074+rge15@users.noreply.github.com>
2021-10-10 19:33:24 +0200

    player Template updated with new enitity data

--- src/resources/templates.s
+++ src/resources/templates.s
@@ -27,7 +27,9 @@ _player_template_e:
    .db #0x00               ; vx
    .db #0x00               ; vy
    .dw #_sprite_player01   ; sprite
+   .dw #0x0000             ; prevptr
    .dw #0x0000             ;ai_behaviour
    .db #0x00               ;ai_counter
    .dw #_man_anim_player   ;animator
    .db #0x10               ;anim. counter
+   .dw #0x0000             ;input_behaviour
