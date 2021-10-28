8404474fbd41ffecb6451cc7a427f0e093f3d032
Edu Gómez <edugomez2299@gmail.com>
2021-10-28 23:28:58 +0100

    [hotfix]: indent levels

--- src/resources/levels.s
+++ src/resources/levels.s
@@ -34,120 +34,152 @@ _level1:
     ; Level 2
     ;================================================================================
 
-    .dw #_tilemap_01            ;Tilemap
-    .db #level_new_entity
-    .dw #t_player
-    .db #42                   ;Entity X
-    .db #176                   ;Entity Y
+   .dw #_tilemap_01            ;Tilemap
+   .db #level_new_entity
+   .dw #t_player
+   .db #42                   ;Entity X
+   .db #176                   ;Entity Y
 
-    .db #level_new_entity
-    .dw #t_enemy_basic_green
-    .db #24
-    .db #156
-    .dw #enemy_no_move
-    .db #4                                 ; e_ai_aux_l
-    .db #48                                 ; e_ai_aux_h
-    .dw #patrol_all_game_zone_m0
-    .dw #_sys_ai_beh_shoot_seekplayer
+   .db #level_new_entity
+   .dw #t_enemy_basic_green
+   .db #24
+   .db #156
+   .dw #enemy_no_move
+   .db #4                                 ; e_ai_aux_l
+   .db #48                                 ; e_ai_aux_h
+   .dw #patrol_all_game_zone_m0
+   .dw #enemy_no_shoot
 
-    ; .db #level_new_entity
-    ; .dw #t_bullet_enemy_l
-    ; .db #50
-    ; .db #50
-    ; .dw #enemy_no_move
-    ; .db #4                                 ; e_ai_aux_l
-    ; .db #48                                 ; e_ai_aux_h
-    ; .dw #patrol_all_game_zone
-    ; .dw #enemy_no_move
-    ;
-    ; .db #level_new_entity
-    ; .dw #t_enemy_basic
-    ; .db #20
-    ; .db #50
-    ; .dw #_sys_ai_behaviourPatrol 
-    ; .db #00                                 ; e_ai_aux_l
-    ; .db #00                                 ; e_ai_aux_h
-    ; ; .dw #patrol_x_50_20 ;; pstrol_step
-    ; .dw #_sys_ai_beh_shoot_x
+   .db #level_new_entity
+   .dw #t_enemy_basic_blue
+   .db #34
+   .db #156
+   .dw #enemy_no_move
+   .db #4                                 ; e_ai_aux_l
+   .db #48                                 ; e_ai_aux_h
+   .dw #patrol_all_game_zone_m0
+   .dw #enemy_no_shoot
 
-    ;; Esquema behaviour follow
-    ; .db #level_new_entity
-    ; .dw #t_enemy_basic
-    ; .db #45
-    ; .db #60
-    ; .dw #_sys_ai_beh_follow_player_y
-    ; ;; si es un timer realmente puede ser cualquier valor
-    ; .db #t_follow_timer
-    ; .db #t_follow_timer
-    ; .dw #patrol_x_50_20
-    ; .dw #_sys_ai_beh_shoot_x_f
+   .db #level_new_entity
+   .dw #t_enemy_basic_purple
+   .db #44
+   .db #156
+   .dw #enemy_no_move
+   .db #4                                 ; e_ai_aux_l
+   .db #48                                 ; e_ai_aux_h
+   .dw #patrol_all_game_zone_m0
+   .dw #enemy_no_shoot
 
-    ;; Esquema patrol relative
-    ; .db #level_new_entity
-    ; .dw #t_enemy_patrol_01
-    ; .db #40
-    ; .db #90
-    ; .dw #_sys_ai_behaviourPatrolRelative
-    ; .db #40
-    ; .db #90                                 ; e_ai_aux_h
-    ; .dw #patrol_relative_x_12
-    ; .dw #_sys_ai_beh_shoot_seekplayer
-    ;
-    ;; Esquema spawner
-    ; TODO: hacer templates para los enemigos a spawnear
-    ; .db #level_new_entity
-    ; .dw #t_spawner_template_02
-    ; .db #55
-    ; .db #90
-    ; .dw #_sys_ai_behaviourSpawner_template
-    ; .db #00                                 ; e_ai_aux_l
-    ; .db #00                                 ; e_ai_aux_h
-    ; .dw #t_enemy_patrol_01
-    ; .dw #enemy_no_shoot
-    ;
-    ; Esquema seeknpatrol
-    ; .db #level_new_entity
-    ; .dw #t_enemy_seeknpatrol
-    ; .db #45
-    ; .db #60
-    ; .dw #_sys_ai_behaviourSeekAndPatrol
-    ; .db #t_follow_timer
-    ; .db #t_follow_timer
-    ; ; .dw #patrol_relative_none
-    ; .dw #patrol_seeknpatrol_01
-    ; .dw #_sys_ai_beh_shoot_xy_rand
+   .db #level_new_entity
+   .dw #t_enemy_basic_red
+   .db #54
+   .db #156
+   .dw #enemy_no_move
+   .db #4                                 ; e_ai_aux_l
+   .db #48                                 ; e_ai_aux_h
+   .dw #patrol_all_game_zone_m0
+   .dw #enemy_no_shoot
 
 
-    .db #level_separator        ;TODO : ESto quitarlo para cargar más niveles
-    ;================================================================================
-    ; Level 2
-    ;================================================================================
 
-    .dw #_tilemap_01            ;Tilemap
-    .db #level_new_entity
-    .dw #t_player
-    .db #0x26                   ;Entity X
-    .db #0xB0                   ;Entity Y
+   ; .db #level_new_entity
+   ; .dw #t_bullet_enemy_l
+   ; .db #50
+   ; .db #50
+   ; .dw #enemy_no_move
+   ; .db #4                                 ; e_ai_aux_l
+   ; .db #48                                 ; e_ai_aux_h
+   ; .dw #patrol_all_game_zone
+   ; .dw #enemy_no_move
+   ;
+   ; .db #level_new_entity
+   ; .dw #t_enemy_basic
+   ; .db #20
+   ; .db #50
+   ; .dw #_sys_ai_behaviourPatrol 
+   ; .db #00                                 ; e_ai_aux_l
+   ; .db #00                                 ; e_ai_aux_h
+   ; ; .dw #patrol_x_50_20 ;; pstrol_step
+   ; .dw #_sys_ai_beh_shoot_x
 
-    ;; spaner bichong
-    .db #level_new_entity
-    .dw #t_spawner_template_02
-    .db #55
-    .db #90
-    .dw #_sys_ai_behaviourSpawner_plist
-    .db #00                                 ; e_ai_aux_l
-    .db #00                                 ; e_ai_aux_h
-    ; .dw #t_enemy_testing
-    .dw #spawner_plist_02
-    .dw #enemy_no_shoot
+   ;; Esquema behaviour follow
+   ; .db #level_new_entity
+   ; .dw #t_enemy_basic
+   ; .db #45
+   ; .db #60
+   ; .dw #_sys_ai_beh_follow_player_y
+   ; ;; si es un timer realmente puede ser cualquier valor
+   ; .db #t_follow_timer
+   ; .db #t_follow_timer
+   ; .dw #patrol_x_50_20
+   ; .dw #_sys_ai_beh_shoot_x_f
 
-    .db #level_separator        ;TODO : ESto quitarlo para cargar más niveles
-    .db #level_separator
-    ; Level 3
+   ;; Esquema patrol relative
+   ; .db #level_new_entity
+   ; .dw #t_enemy_patrol_01
+   ; .db #40
+   ; .db #90
+   ; .dw #_sys_ai_behaviourPatrolRelative
+   ; .db #40
+   ; .db #90                                 ; e_ai_aux_h
+   ; .dw #patrol_relative_x_12
+   ; .dw #_sys_ai_beh_shoot_seekplayer
+   ;
+   ;; Esquema spawner
+   ; TODO: hacer templates para los enemigos a spawnear
+   ; .db #level_new_entity
+   ; .dw #t_spawner_template_02
+   ; .db #55
+   ; .db #90
+   ; .dw #_sys_ai_behaviourSpawner_template
+   ; .db #00                                 ; e_ai_aux_l
+   ; .db #00                                 ; e_ai_aux_h
+   ; .dw #t_enemy_patrol_01
+   ; .dw #enemy_no_shoot
+   ;
+   ; Esquema seeknpatrol
+   ; .db #level_new_entity
+   ; .dw #t_enemy_seeknpatrol
+   ; .db #45
+   ; .db #60
+   ; .dw #_sys_ai_behaviourSeekAndPatrol
+   ; .db #t_follow_timer
+   ; .db #t_follow_timer
+   ; ; .dw #patrol_relative_none
+   ; .dw #patrol_seeknpatrol_01
+   ; .dw #_sys_ai_beh_shoot_xy_rand
 
-    .db #level_separator
 
-    ;================================================================================
-    ; GAME END
-    ;================================================================================
-    .db #level_separator
+   .db #level_separator        ;TODO : ESto quitarlo para cargar más niveles
+   ;================================================================================
+   ; Level 2
+   ;================================================================================
+
+   .dw #_tilemap_01            ;Tilemap
+   .db #level_new_entity
+   .dw #t_player
+   .db #0x26                   ;Entity X
+   .db #0xB0                   ;Entity Y
+
+   ;; spaner bichong
+   .db #level_new_entity
+   .dw #t_spawner_template_02
+   .db #55
+   .db #90
+   .dw #_sys_ai_behaviourSpawner_plist
+   .db #00                                 ; e_ai_aux_l
+   .db #00                                 ; e_ai_aux_h
+   ; .dw #t_enemy_testing
+   .dw #spawner_plist_02
+   .dw #enemy_no_shoot
+
+   .db #level_separator        ;TODO : ESto quitarlo para cargar más niveles
+   .db #level_separator
+   ; Level 3
+
+   .db #level_separator
+
+   ;================================================================================
+   ; GAME END
+   ;================================================================================
+   .db #level_separator
