6d8512359de597044bfe3d2f551977237b2c0cdd
Edu Gómez <edugomez2299@gmail.com>
2021-10-28 22:42:06 +0100

    [feature]: more tilemap

--- src/resources/tilemaps.h.s
+++ src/resources/tilemaps.h.s
@@ -4,6 +4,9 @@
 .globl _tilemap_01
 .globl _tilemap_02
 .globl _tilemap_03
+.globl _tilemap_03
+.globl _tilemap_04
+.globl _tilemap_05
 .globl _tilemap_06
 
 ;================================================================================
