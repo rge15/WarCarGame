9865fdebd7a19f06d50814c5a4ca75c21aaf8e64
YaroslavPD <yp13@alu.ua.es>
2021-11-02 19:19:28 +0100

    He puesto el copyright ese

--- src/resources/macros.s
+++ src/resources/macros.s
@@ -1,3 +1,22 @@
+;;-----------------------------LICENSE NOTICE------------------------------------
+;;  This file is part of Aliens'n Tanks: An Amstrad CPC Game 
+;;  Copyright (C) 2021 Yaroslav Paslavskiy Danko / Rodrigo Guzmán Escribá / 
+;;                     Eduardo David Gómez Saldias / Semag Ohcaco (@SemagOhcaco)
+;;
+;;  This program is free software: you can redistribute it and/or modify
+;;  it under the terms of the GNU Lesser General Public License as published by
+;;  the Free Software Foundation, either version 3 of the License, or
+;;  (at your option) any later version.
+;;
+;;  This program is distributed in the hope that it will be useful,
+;;  but WITHOUT ANY WARRANTY; without even the implied warranty of
+;;  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;;  GNU Lesser General Public License for more details.
+;;
+;;  You should have received a copy of the GNU Lesser General Public License
+;;  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+;;------------------------------------------------------------------------------
+
 .module Macros
 .include "resources/macros.h.s"
 
